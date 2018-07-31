41278ac71ad0dcfe0054569c7541b87a777ef4e7
paran_son <paran_son@outlook.com>
2018-07-31 14:19:20 -0700

    inception_v3

---
+++ retrain_run_inference.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+
+imagePath = 'test.jpg'                                      # 추론을 진행할 이미지 경로
+modelFullPath = '/tmp/output_graph.pb'                       # 읽어들일 graph 파일 경로
+labelsFullPath = '/tmp/output_labels.txt'                         # 읽어들일 labels 파일 경로
+
+def run_inference_on_image():
+    answer = None
+
+    if not tf.gfile.Exists(imagePath):
+        tf.logging.fatal('File does not exist %s', imagePath)
+        return answer
+
+    image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
+
+    with tf.gfile.FastGFile(modelFullPath, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        top_k = predictions.argsort()[-5:][::-1]  # get predictions high top 5
+        f = open(labelsFullPath, 'rb')
+        lines = f.readlines()
+        labels = [str(w).replace("\n", "") for w in lines]
+        for node_id in top_k:
+            human_string = labels[node_id]
+            score = predictions[node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+            if 'car' in human_string:
+                if score > 0.7:
+                    print('자동차 사진입니다.')
+                else
+                    print('자동차 사진이 아닙니다.'')
+        answer = labels[top_k[0]]
+        return answer
+
+run_inference_on_image()
